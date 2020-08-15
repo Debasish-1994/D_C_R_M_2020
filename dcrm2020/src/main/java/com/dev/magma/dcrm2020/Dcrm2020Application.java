c6a0bb7f70b18503f4cd8d0e13557fe330de5318
Debasish <debasishpradhan890@gmail.com>
2020-08-15 18:10:37 +0530

    Project Setup Done

---
+++ dcrm2020/src/main/java/com/dev/magma/dcrm2020/Dcrm2020Application.java
@@ -0,0 +1,13 @@
+package com.dev.magma.dcrm2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dcrm2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dcrm2020Application.class, args);
+	}
+
+}
---
+++ dcrm2020/src/test/java/com/dev/magma/dcrm2020/Dcrm2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.magma.dcrm2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dcrm2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
