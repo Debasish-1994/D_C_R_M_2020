3804185d70c0ab4f43f77ea78a1eec429645c854
91943 <91943@DESKTOP-RCGCUMI>
2020-08-15 19:51:10 +0530

    Commited all changes

--- dcrm2020/src/main/java/com/dev/magma/dcrm2020/controller/LoginController.java
+++ dcrm2020/src/main/java/com/dev/magma/dcrm2020/controller/LoginController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dev.magma.dcrm2020.model.AuthenticateResponse;
 import com.dev.magma.dcrm2020.model.AuthenticationRequest;
 import com.dev.magma.dcrm2020.model.CompanyDetails;
+import com.dev.magma.dcrm2020.model.OwnerDetails;
 import com.dev.magma.dcrm2020.model.ProjectDetails;
 import com.dev.magma.dcrm2020.model.UserDetails;
-
 
 @RestController
 public class LoginController {
@@ -22,25 +22,31 @@ public class LoginController {
 		System.out.println(authenticationRequest.toString());
 		
 		UserDetails userDetails = new UserDetails();
-		userDetails.setEmailId("debasish.p@wawa.ru");
-		userDetails.setFirstName("Debasish");
+		userDetails.setEmailId("sandeep.d@gmil.com");
+		userDetails.setFirstName("Sandeep");
 		userDetails.setMiddleName("");
-		userDetails.setLastName("Pradhan");
-		userDetails.setPassword("lalalala");
+		userDetails.setLastName("Dash");
+		userDetails.setPassword("Sandeep123");
 		userDetails.setUserId(57);
-		userDetails.setUserName("debasish0057");
-		userDetails.setUserRole("admin");
+		userDetails.setUserName("sandeep");
+		userDetails.setUserRole("SuperAdmin");
 		
 		CompanyDetails companyDetails = new CompanyDetails();
-		companyDetails.setCompanyName("WL FL Off Shop");
-		companyDetails.setLogo("kingfisher/logo");
+		companyDetails.setCompanyName("Magma");
+		companyDetails.setLogo("//logo");
 		
 		ProjectDetails projectDetails = new ProjectDetails();
-		projectDetails.setProjectDesc("something");
+		projectDetails.setProjectDesc("DCRM is in progress");
 		projectDetails.setProjectId(1);
-		projectDetails.setProjectName("magura khia");
+		projectDetails.setProjectName("DCRM");
 		
-		AuthenticateResponse authenticateResponse = new AuthenticateResponse(userDetails, companyDetails, projectDetails);
+		OwnerDetails ownerDetails = new OwnerDetails();
+		ownerDetails.setOwner_name("Sandeep Dash");
+		ownerDetails.setOwner_email("sandeep.d@gmail.com");
+		ownerDetails.setContact_info("1234567890");
+		ownerDetails.setProfile_pic("url");
+		
+		AuthenticateResponse authenticateResponse = new AuthenticateResponse(userDetails, companyDetails, projectDetails, ownerDetails);
 		return ResponseEntity.ok(authenticateResponse);
 	}
 }
--- dcrm2020/src/main/java/com/dev/magma/dcrm2020/model/AuthenticateResponse.java
+++ dcrm2020/src/main/java/com/dev/magma/dcrm2020/model/AuthenticateResponse.java
@@ -10,12 +10,14 @@ public class AuthenticateResponse implements Serializable {
 	private UserDetails userDetails;
 	private CompanyDetails companyDetails;
 	private ProjectDetails projectDetails;
+	private OwnerDetails ownerDetails;
 
 	public AuthenticateResponse(UserDetails userDetails, CompanyDetails companyDetails,
-			ProjectDetails projectDetails) {
+			ProjectDetails projectDetails, OwnerDetails ownerDetails) {
 		this.userDetails = userDetails;
 		this.companyDetails = companyDetails;
 		this.projectDetails = projectDetails;
+		this.ownerDetails = ownerDetails;
 	}
 
 	public UserDetails getUserDetails() {
@@ -42,11 +44,20 @@ public class AuthenticateResponse implements Serializable {
 		this.projectDetails = projectDetails;
 	}
 
+	public OwnerDetails getOwnerDetails() {
+		return ownerDetails;
+	}
+
+	public void setOwnerDetails(OwnerDetails ownerDetails) {
+		this.ownerDetails = ownerDetails;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((companyDetails == null) ? 0 : companyDetails.hashCode());
+		result = prime * result + ((ownerDetails == null) ? 0 : ownerDetails.hashCode());
 		result = prime * result + ((projectDetails == null) ? 0 : projectDetails.hashCode());
 		result = prime * result + ((userDetails == null) ? 0 : userDetails.hashCode());
 		return result;
@@ -66,6 +77,11 @@ public class AuthenticateResponse implements Serializable {
 				return false;
 		} else if (!companyDetails.equals(other.companyDetails))
 			return false;
+		if (ownerDetails == null) {
+			if (other.ownerDetails != null)
+				return false;
+		} else if (!ownerDetails.equals(other.ownerDetails))
+			return false;
 		if (projectDetails == null) {
 			if (other.projectDetails != null)
 				return false;
@@ -82,9 +98,7 @@ public class AuthenticateResponse implements Serializable {
 	@Override
 	public String toString() {
 		return "AuthenticateResponse [userDetails=" + userDetails + ", companyDetails=" + companyDetails
-				+ ", projectDetails=" + projectDetails + "]";
+				+ ", projectDetails=" + projectDetails + ", ownerDetails=" + ownerDetails + "]";
 	}
-
-	
 
 }
