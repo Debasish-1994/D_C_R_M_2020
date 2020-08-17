16a9e53ed1c5b3ef8182ae1472ad4c9202568cf6
Debasish <debasishpradhan890@gmail.com>
2020-08-18 00:22:59 +0530

    id getter setter added.

--- dcrm2020/src/main/java/com/dev/magma/dcrm2020/model/CompanyDetails.java
+++ dcrm2020/src/main/java/com/dev/magma/dcrm2020/model/CompanyDetails.java
@@ -23,6 +23,14 @@ public class CompanyDetails implements Serializable {
 	@Column(name = "logo_url")
 	private String logo;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getCompanyName() {
 		return companyName;
 	}
